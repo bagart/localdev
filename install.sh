e3c2308114e6a10fdc339d42710e18b1b99ff889
baltaev.artur <baltaev.artur@gmail.com>
2018-03-22 04:08:35 +0300

    deploy

--- install.sh
+++ install.sh
@@ -6,10 +6,4 @@ source get_minikube.sh
 source get_kubectl.sh
 source get_helm.sh
 
-## Install kubernetes using minikube
-source install_kubernetes.sh
-
-helm init
-sleep 70
-echo "== Installing defined services =="
-source install_services.sh
+source deploy.sh
