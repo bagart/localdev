8e2a56f4e832511f48a3f5d2c7744002ebe4e72e
baltaev.artur <baltaev.artur@gmail.com>
2018-03-23 12:55:36 +0300

    test fix

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ source install_kubernetes.sh
 echo -=-=-=- starting helm init -=-=-=-
 helm init
 
-echo -=-=-=- sleep 20 -=-=-=-
-sleep 20
+echo -=-=-=- sleep 70 -=-=-=-
+sleep 70
 
 echo -=-=-=- starting install_services -=-=-=-
 source install_services.sh
