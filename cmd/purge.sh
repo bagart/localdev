3d1c13303ff86c8abeda9b254ca31dad21a7ce49
baltaev.artur <baltaev.artur@gmail.com>
2018-03-30 18:48:40 +0300

    cmd

--- cmd/purge.sh
+++ cmd/purge.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo minikube delete
 
-rm -rf ~/.minikube/ ~/.kube ~/.localdev ~/.hel
+rm -rf ~/.minikube/ ~/.kube ~/.localdev ~/.helm
 
 echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
 echo nameserver 8.8.4.4 | sudo tee --append /etc/resolv.conf
