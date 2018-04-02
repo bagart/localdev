a48069b12f1ae1cc6eca73499c792f24ab644ba3
baltaev.artur <baltaev.artur@gmail.com>
2018-04-02 20:12:11 +0300

    kube console connect

--- cmd/console.sh
+++ cmd/console.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
-
+SERVICE_NAMESPACE="localdev"
 if [[ "$1" == "" ]]; then
-    kubectl get pods --all-namespaces | grep localdev | grep -v redis
-    echo "use cmd/console.sh ext-api";
+    kubectl get pods --all-namespaces | grep $SERVICE_NAMESPACE | grep -v redis | grep -v postgres
+    echo "use cmd/console.sh infra";
 else
-    KUBE_NAME=`kubectl get pods --all-namespaces | grep $1 | grep -v redis | sed 's/^[^\s]\+\s\+//' | sed 's/\s.*//'`
-    kubectl -n localdev exec -ti $KUBE_NAME -c phpfpm bash
+	    #echo "kubectl get pods --all-namespaces | grep $SERVICE_NAMESPACE | grep $1 | grep -v redis | grep -v postgres | sed 's/^[^ ]\+ \+//' | sed 's/\s.*//'";
+    KUBE_NAME=`kubectl get pods --all-namespaces | grep $SERVICE_NAMESPACE | grep $1 | grep -v redis | grep -v postgres | sed 's/^[^ ]\+ \+//' | sed 's/\s.*//'`
+	echo connect to $KUBE_NAME
+    kubectl -n $SERVICE_NAMESPACE exec -ti $KUBE_NAME -c phpfpm bash
 fi
-
